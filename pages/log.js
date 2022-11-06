e7568a200e3828cb18460ee206351789e897aa7d
brycebohling <96216764+brycebohling@users.noreply.github.com>
2022-11-06 15:38:53 -0800

    log work

--- pages/log.js
+++ pages/log.js
@@ -1,7 +1,38 @@
+import { Button, Form, Input, Rate } from 'antd';
+import Page from '../components/Page';
+
+const { TextArea } = Input;
 
 function Log() {
+    const [form] = Form.useForm();
 
-    return <h1>Log Page</h1>
+    const onFinish = (values) => {
+        console.log(values);
+      };
+    
+    const onReset = () => {
+    form.resetFields();
+    };   
+    
+    return <Page title="Log Today">
+        <Form form={form} name="control-hooks" onFinish={onFinish} layout="vertical">
+        <Form.Item name="rating" label="rating" rules={[{ required: true }]}>
+            <Rate allowHalf  />
+            </Form.Item>
+            <Form.Item name="comments" label="Comments" rules={[{ required: false }]}>
+            <TextArea
+            // value={value}
+            // onChange={e => setValue(e.target.value)}
+            placeholder="Blah blah..."
+            autoSize={{ minRows: 3, maxRows: 5 }} />
+            </Form.Item>
+            <Form.Item>
+            <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        </Form.Item>
+        </Form>    
+    </Page>
 }
 
 export default Log;
